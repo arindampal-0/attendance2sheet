2f8ca46a05d1f26ac6d722885f27dc8798ede012
Arindam Pal <arindampal2000@gmail.com>
2026-09-04 16:27:07 +0530

    streamlit upload images

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ def main() -> int:
     """main function"""
     print("Attendance2Sheet")
 
-    streamlit.text("Attendance2Sheet")
+    streamlit.title("Attendance2Sheet")
+    streamlit.header("Upload the attendance sheet photo")
+    image_data = streamlit.file_uploader(
+        "Upload the attendance sheet image", 
+        type="image/*",
+        max_upload_size=10,
+        accept_multiple_files=True,
+    )
+    # print(image_data)
+    if len(image_data) > 0:
+        for image in image_data:
+            streamlit.image(image)
 
     return 0
 
