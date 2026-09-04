616c9ea51fac3332f1bb34ab58ec740f045ebf33
Arindam Pal <arindampal2000@gmail.com>
2026-09-04 15:31:04 +0530

    basic streamlit app

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Attendance2Sheet
+
+## Instructions
+
+Run the application
+```powershell
+# activate the environment
+.venv\Scripts\Activate.ps1
+
+# run the streamlit app
+streamlit run main.py
+```
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 """main file"""
 import sys
 
+import streamlit
+
 def main() -> int:
     """main function"""
     print("Attendance2Sheet")
+
+    streamlit.text("Attendance2Sheet")
+
     return 0
 
 if __name__ == "__main__":
