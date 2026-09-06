7f28367b7740b756a097a2399d300f1748232bba
Arindam Pal <arindampal2000@gmail.com>
2026-09-06 11:05:11 +0530

    Added UI in streamlit app to prompt gemini model and get response

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # Attendance2Sheet
 
 ## Instructions
+Create virtual environment and install dependencies
+```powershell
+python -m venv .venv
+.venv\Scripts\Activate.ps1
+pip install -r requirements.txt
+```
+
+Add Gemini API key
+```powershell
+$env:GEMINI_API_KEY = "your api key here"
+```
 
 Run the application
 ```powershell
-# activate the environment
+# activate the environment if not already activated
 .venv\Scripts\Activate.ps1
 
 # run the streamlit app
--- main.py
+++ main.py
@@ -2,25 +2,52 @@
 import sys
 
 import streamlit
+from google import genai
+@streamlit.cache_resource
+def get_gemini_client():
+    """create gemini client"""
+    return genai.Client()
 
-def main() -> int:
-    """main function"""
-    print("Attendance2Sheet")
+client = get_gemini_client()
+available_models = ["gemini-3.7-flash", "gemini-3.6-flash"]
 
-    streamlit.title("Attendance2Sheet")
-    streamlit.header("Upload the attendance sheet photo")
-    image_data = streamlit.file_uploader(
-        "Upload the attendance sheet image", 
-        type="image/*",
-        max_upload_size=10,
-        accept_multiple_files=True,
+@streamlit.cache_resource(scope="session")
+def generate_response(prompt: str, model: str) -> genai.types.GenerateContentResponse:
+    """generate response"""
+    return client.models.generate_content(
+        model=model,
+        contents=[prompt]
     )
-    # print(image_data)
-    if len(image_data) > 0:
-        for image in image_data:
-            streamlit.image(image)
 
-    return 0
+streamlit.title("Attendance2Sheet")
+streamlit.header("Upload the attendance sheet photo")
+image_data = streamlit.file_uploader(
+    "Upload the attendance sheet image", 
+    type="image/*",
+    max_upload_size=10,
+    accept_multiple_files=True,
+)
 
-if __name__ == "__main__":
-    sys.exit(main())
+# print(image_data)
+if len(image_data) > 0:
+    for image in image_data:
+        print(image)
+        print(image.type)
+        streamlit.image(image)
+
+selected_model = streamlit.selectbox("Select model", available_models, index=0)
+streamlit.write(selected_model)
+
+input_prompt = streamlit.text_area("Enter your prompt")
+send_button = streamlit.button("Send")
+streamlit.write(input_prompt)
+streamlit.write(send_button)
+
+if send_button and input_prompt and \
+    isinstance(selected_model, str) and selected_model in available_models:
+    streamlit.write("button pressed")
+    response = generate_response(input_prompt, selected_model)
+    streamlit.write(response)
+    if response.text:
+        streamlit.divider()
+        streamlit.markdown(response.text)
